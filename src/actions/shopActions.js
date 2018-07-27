1575eceeab3c5898b53c567d2201473237a5d9b5
josemirkin <jsoemirkin@gmail.com>
2018-07-27 07:21:21 -0300

    en teoria anda, le falta swag y total

--- src/actions/shopActions.js
+++ src/actions/shopActions.js
@@ -1,12 +1,22 @@
 import actionTypes from '../constants/actionTypes';
 
-const addProduct = (name, description, price) => ({
+const addProduct = (name, price) => ({
 	type: actionTypes.SHOP_ADD_PRODUCT,
 	payload: {
 		name,
-		description,
 		price
 	}
 });
 
-export default { addProduct };
+const removeAllProduct = (item) => ({
+	type: actionTypes.SHOP_REMOVE_ALL_PRODUCT,
+	payload: item
+});
+
+const removeProduct = (item) => ({
+	type: actionTypes.SHOP_REMOVE_PRODUCT,
+	payload: item
+});
+
+
+export default { addProduct, removeAllProduct, removeProduct };
